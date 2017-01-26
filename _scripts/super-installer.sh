ab7930be189a4ba121d019427ad7019fbd720ae4
Ryan Tablada <ryan.tablada@gmail.com>
2017-01-26 13:38:02 -0600

    Yeah...

--- _scripts/super-installer.sh
+++ _scripts/super-installer.sh
@@ -65,7 +65,7 @@ echo "Setting up Git Ignore"
 wget https://gist.githubusercontent.com/octocat/9257657/raw/3f9569e65df83a7b328b39a091f0ce9c6efc6429/.gitignore -O ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
-# Let's make the Terminal Great AGAIN!
+# Let's improve the terminal
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 mkdir ~/bin
